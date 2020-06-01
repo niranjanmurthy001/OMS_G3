fb542d1ce917ed75c40203063ce9e90e2e8b0fe9
niranjanmurthy001 <niranjanmurthy@drnds.com>
2020-06-01 15:20:14 +0530

    Changes by-  bindu

---
+++ Ordermanagement_01/Opp/Opp_Master/Order_SourceType_View.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Ordermanagement_01.Opp.Opp_Master
+{
+    partial class Order_SourceType_View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_Add_NewSource = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_Delete_MultipleSource = new DevExpress.XtraEditors.SimpleButton();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.grd_SourceType = new DevExpress.XtraGrid.GridControl();
+            this.gridViewSource = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn8 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemHyperLinkEdit = new DevExpress.XtraEditors.Repository.RepositoryItemHyperLinkEdit();
+            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemHyperLinkDelete = new DevExpress.XtraEditors.Repository.RepositoryItemHyperLinkEdit();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grd_SourceType)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemHyperLinkEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemHyperLinkDelete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2013";
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Appearance.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.groupControl1.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.groupControl1.Appearance.Options.UseFont = true;
+            this.groupControl1.Appearance.Options.UseForeColor = true;
+            this.groupControl1.AppearanceCaption.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.groupControl1.AppearanceCaption.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.groupControl1.AppearanceCaption.Options.UseFont = true;
+            this.groupControl1.AppearanceCaption.Options.UseForeColor = true;
+            this.groupControl1.Controls.Add(this.tableLayoutPanel1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(649, 750);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "Order Source Type View";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panelControl1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 21);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.427785F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.57221F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(645, 727);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btn_Add_NewSource);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Delete_MultipleSource);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(639, 47);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // btn_Add_NewSource
+            // 
+            this.btn_Add_NewSource.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add_NewSource.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.btn_Add_NewSource.Appearance.Options.UseFont = true;
+            this.btn_Add_NewSource.Appearance.Options.UseForeColor = true;
+            this.btn_Add_NewSource.Location = new System.Drawing.Point(556, 3);
+            this.btn_Add_NewSource.Name = "btn_Add_NewSource";
+            this.btn_Add_NewSource.Size = new System.Drawing.Size(80, 40);
+            this.btn_Add_NewSource.TabIndex = 3;
+            this.btn_Add_NewSource.Text = "Add New";
+            this.btn_Add_NewSource.Click += new System.EventHandler(this.btn_Add_NewSource_Click);
+            // 
+            // btn_Delete_MultipleSource
+            // 
+            this.btn_Delete_MultipleSource.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete_MultipleSource.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.btn_Delete_MultipleSource.Appearance.Options.UseFont = true;
+            this.btn_Delete_MultipleSource.Appearance.Options.UseForeColor = true;
+            this.btn_Delete_MultipleSource.Location = new System.Drawing.Point(470, 3);
+            this.btn_Delete_MultipleSource.Name = "btn_Delete_MultipleSource";
+            this.btn_Delete_MultipleSource.Size = new System.Drawing.Size(80, 40);
+            this.btn_Delete_MultipleSource.TabIndex = 1;
+            this.btn_Delete_MultipleSource.Text = "Delete";
+            this.btn_Delete_MultipleSource.Click += new System.EventHandler(this.btn_Delete_MultipleSource_Click);
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.grd_SourceType);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(3, 56);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(639, 668);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // grd_SourceType
+            // 
+            this.grd_SourceType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grd_SourceType.Location = new System.Drawing.Point(2, 2);
+            this.grd_SourceType.MainView = this.gridViewSource;
+            this.grd_SourceType.Name = "grd_SourceType";
+            this.grd_SourceType.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemHyperLinkEdit,
+            this.repositoryItemHyperLinkDelete});
+            this.grd_SourceType.Size = new System.Drawing.Size(635, 664);
+            this.grd_SourceType.TabIndex = 0;
+            this.grd_SourceType.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridViewSource});
+            // 
+            // gridViewSource
+            // 
+            this.gridViewSource.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3,
+            this.gridColumn8,
+            this.gridColumn7,
+            this.gridColumn4,
+            this.gridColumn5,
+            this.gridColumn6});
+            this.gridViewSource.GridControl = this.grd_SourceType;
+            this.gridViewSource.IndicatorWidth = 30;
+            this.gridViewSource.Name = "gridViewSource";
+            this.gridViewSource.OptionsFind.AlwaysVisible = true;
+            this.gridViewSource.OptionsSelection.CheckBoxSelectorColumnWidth = 38;
+            this.gridViewSource.OptionsSelection.InvertSelection = true;
+            this.gridViewSource.OptionsSelection.MultiSelect = true;
+            this.gridViewSource.OptionsSelection.MultiSelectMode = DevExpress.XtraGrid.Views.Grid.GridMultiSelectMode.CheckBoxRowSelect;
+            this.gridViewSource.OptionsSelection.ShowCheckBoxSelectorInGroupRow = DevExpress.Utils.DefaultBoolean.True;
+            this.gridViewSource.CustomDrawRowIndicator += new DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventHandler(this.gridViewSource_CustomDrawRowIndicator);
+            this.gridViewSource.SelectionChanged += new DevExpress.Data.SelectionChangedEventHandler(this.gridViewSource_SelectionChanged);
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "Project Type";
+            this.gridColumn1.FieldName = "Project_Type";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.OptionsColumn.AllowEdit = false;
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 1;
+            this.gridColumn1.Width = 76;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "Product Type";
+            this.gridColumn2.FieldName = "Product_Type";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.OptionsColumn.AllowEdit = false;
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 2;
+            this.gridColumn2.Width = 143;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "Source ";
+            this.gridColumn3.FieldName = "Employee_source";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.OptionsColumn.AllowEdit = false;
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 3;
+            this.gridColumn3.Width = 207;
+            // 
+            // gridColumn8
+            // 
+            this.gridColumn8.Caption = "Edit";
+            this.gridColumn8.ColumnEdit = this.repositoryItemHyperLinkEdit;
+            this.gridColumn8.Name = "gridColumn8";
+            this.gridColumn8.Visible = true;
+            this.gridColumn8.VisibleIndex = 4;
+            this.gridColumn8.Width = 47;
+            // 
+            // repositoryItemHyperLinkEdit
+            // 
+            this.repositoryItemHyperLinkEdit.AccessibleName = "Edit";
+            this.repositoryItemHyperLinkEdit.AutoHeight = false;
+            this.repositoryItemHyperLinkEdit.Caption = "Edit";
+            this.repositoryItemHyperLinkEdit.Name = "repositoryItemHyperLinkEdit";
+            this.repositoryItemHyperLinkEdit.NullText = "Edit";
+            this.repositoryItemHyperLinkEdit.Click += new System.EventHandler(this.repositoryItemHyperLinkEdit_Click);
+            // 
+            // gridColumn7
+            // 
+            this.gridColumn7.Caption = "Delete";
+            this.gridColumn7.ColumnEdit = this.repositoryItemHyperLinkDelete;
+            this.gridColumn7.Name = "gridColumn7";
+            this.gridColumn7.Visible = true;
+            this.gridColumn7.VisibleIndex = 5;
+            this.gridColumn7.Width = 50;
+            // 
+            // repositoryItemHyperLinkDelete
+            // 
+            this.repositoryItemHyperLinkDelete.AccessibleName = "Delete";
+            this.repositoryItemHyperLinkDelete.AutoHeight = false;
+            this.repositoryItemHyperLinkDelete.Caption = "Delete";
+            this.repositoryItemHyperLinkDelete.Name = "repositoryItemHyperLinkDelete";
+            this.repositoryItemHyperLinkDelete.NullText = "Delete";
+            this.repositoryItemHyperLinkDelete.Click += new System.EventHandler(this.repositoryItemHyperLinkDelete_Click);
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "Project_Type_Id";
+            this.gridColumn4.FieldName = "Project_Type_Id";
+            this.gridColumn4.Name = "gridColumn4";
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "ProductType_Id";
+            this.gridColumn5.FieldName = "ProductType_Id";
+            this.gridColumn5.Name = "gridColumn5";
+            // 
+            // gridColumn6
+            // 
+            this.gridColumn6.Caption = "Employee_Source_id";
+            this.gridColumn6.FieldName = "Employee_Source_id";
+            this.gridColumn6.Name = "gridColumn6";
+            // 
+            // Order_SourceType_View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 750);
+            this.Controls.Add(this.groupControl1);
+            this.Name = "Order_SourceType_View";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Order_SourceType_View";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Order_SourceType_View_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grd_SourceType)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemHyperLinkEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemHyperLinkDelete)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraGrid.GridControl grd_SourceType;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridViewSource;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private DevExpress.XtraEditors.SimpleButton btn_Delete_MultipleSource;
+        private DevExpress.XtraEditors.SimpleButton btn_Add_NewSource;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn8;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
+        private DevExpress.XtraEditors.Repository.RepositoryItemHyperLinkEdit repositoryItemHyperLinkEdit;
+        private DevExpress.XtraEditors.Repository.RepositoryItemHyperLinkEdit repositoryItemHyperLinkDelete;
+    }
+}
