a88a96760b9af9f4e90815d74020e1b184b7aabf
niranjanmurthy001 <niranjanmurthy@drnds.com>
2020-07-29 23:53:49 +0530

    changes by - bindu

--- Ordermanagement_01/Program.cs
+++ Ordermanagement_01/Program.cs
@@ -20,7 +20,7 @@ namespace Ordermanagement_01
             // Application.Run(new Vendors.Keywords(1));
             //Application.Run(new Matrix.Client_Target_Matrix(1,"1"));
             //Application.Run(new DailyStatusReport_Preview(1,"2",""));
-          //   Application.Run(new New_Dashboard.NewLogin());
+            //   Application.Run(new New_Dashboard.NewLogin());
             // Application.Run(new New_Dashboard.Settings.Clarification_Setting(1,"1"));
             // Application.Run(new Check_List_New(1, 1, 1, "2", "1", "2", 209816, 0, 0, "565172", "", "", 2, 1, "565172", "2", 1));
             //Application.Run(new Order_Document_List(1, 202696, 4,1));
@@ -38,15 +38,15 @@ namespace Ordermanagement_01
             //Application.Run(new Opp.Opp_Master.ImportErrorInfo("Error Field",));
             //Application.Run(new Opp.Opp_Efficiency.Efficiency_View(1));
             // Application.Run(new Opp.Opp_Master.Sub_Product_Type_View(3));
-           // Application.Run(new Opp.Opp_Master.Order_SourceType_View(3));
+            // Application.Run(new Opp.Opp_Master.Order_SourceType_View(3));
             //Application.Run(new Opp.Opp_Master.Error_Settings());
             // Application.Run(new Opp.Opp_Efficiency.Efficiency_Order_Source_Type(1));
             //Application.Run(new Opp.Opp_Master.ProjectType_Notification_Settings_View(1));
             //Application.Run(new Opp.Opp_Master.Project_Wise_Notification_View(1,1));
             // Application.Run(new New_Dashboard.Employee.General_Notification(2, 1));
-            //Application.Run(new Opp.Opp_Efficiency.Client_TAT_View(3,"2"));
-            Application.Run(new Opp.Opp_CheckList.CheckLists(1,1,2,200226,5,6,2,1));
-          //  Application.Run(new Opp.Opp_CheckList.Checklist_Settings_View());
+            //  Application.Run(new Opp.Opp_Efficiency.Client_TAT_View(3,"2"));
+            // Application.Run(new Opp.Opp_CheckList.CheckLists(1,1,2,200226,5,6,2,1));
+          //   Application.Run(new Opp.Opp_CheckList.CheckList(1,1,2,200226,5,6,2,1));
             //Application.Run(new Opp.Opp_CheckList.CheckList_Master_View());
         }
 
